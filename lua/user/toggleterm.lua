b3b985ccde39a80387be5b0be029a64821741513
Qadis Chaudhry <qadis.chaudhry@gmail.com>
2022-03-12 21:09:03 -0500

    minor changes

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -5,39 +5,39 @@ end
 
 toggleterm.setup({
     size = 20,
-    open_mapping = [[<c-\>]],
-	hide_numbers = true,
-	shade_filetypes = {},
-	shade_terminals = true,
-	shading_factor = 2,
-	start_in_insert = true,
-	insert_mappings = true,
-	persist_size = true,
-	direction = "float",
-	close_on_exit = true,
-	shell = vim.o.shell,
-	float_opts = {
-		border = "curved",
-		winblend = 0,
-		highlights = {
-			border = "Normal",
-			background = "Normal",
-		},
-	},
+    open_mapping = [[<C-\>]],
+    hide_numbers = true,
+    shade_filetypes = {},
+    shade_terminals = true,
+    shading_factor = 2,
+    start_in_insert = true,
+    insert_mappings = true,
+    persist_size = true,
+    direction = "float",
+    close_on_exit = true,
+    shell = vim.o.shell,
+    float_opts = {
+        border = "curved",
+        winblend = 0,
+        highlights = {
+            border = "Normal",
+            background = "Normal",
+        },
+    },
 })
 
 local Terminal = require("toggleterm.terminal").Terminal
 
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 function _LAZYGIT_TOGGLE()
-	lazygit:toggle()
+    lazygit:toggle()
 end
 
 vim.api.nvim_set_keymap("n", "<leader>ig", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", {noremap = true, silent = true})
 
 local python = Terminal:new({ cmd = "ipython", hidden = true })
 function _PYTHON_TOGGLE()
-	python:toggle()
+    python:toggle()
 end
 
 vim.api.nvim_set_keymap("n", "<leader>ip", "<cmd>lua _PYTHON_TOGGLE()<CR>", {noremap = true, silent = true})
