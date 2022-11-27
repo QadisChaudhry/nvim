bb65b334e046c936ee6337c616c9366aa360b5d1
Qadis Chaudhry <qadis.chaudhry@gmail.com>
2022-11-27 18:04:06 -0500

    changes to bufferline

--- lua/user/heirline.lua
+++ lua/user/heirline.lua
@@ -341,6 +341,142 @@ local statusline = {
 }
 
 local winbar = {}
-local tabline = {}
 
-heirline.setup(statusline)
+local TablineFileFlags = {
+    {
+        condition = function(self)
+            return vim.api.nvim_buf_get_option(self.bufnr, "modified")
+        end,
+        provider = "    ",
+        hl = { fg = "cyan" },
+    },
+    {
+        condition = function(self)
+            return not vim.api.nvim_buf_get_option(self.bufnr, "modified")
+        end,
+        provider = "    ",
+        hl = { fg = "cyan" },
+        on_click = {
+            callback = function(_, minwid)
+                vim.api.nvim_buf_delete(minwid, { force = false })
+            end,
+            minwid = function(self)
+                return self.bufnr
+            end,
+            name = "heirline_tabline_close_buffer_callback",
+        },
+    },
+    {
+        condition = function(self)
+            return not vim.api.nvim_buf_get_option(self.bufnr, "modifiable")
+                or vim.api.nvim_buf_get_option(self.bufnr, "readonly")
+        end,
+        provider = function(self)
+            if vim.api.nvim_buf_get_option(self.bufnr, "buftype") == "terminal" then
+                return "  "
+            else
+                return "    "
+            end
+        end,
+        hl = { fg = "cyan" },
+    },
+}
+
+local TablineFileIcon = {
+    provider = function(self)
+        local fileicon = self.fileicon
+        return fileicon
+    end,
+    hl = function(self)
+        return { fg = self.icon_color }
+    end
+}
+
+local TablineFileName = {
+    provider = function(self)
+        local filename = self.filename
+        filename = filename == "" and "[No Name]" or vim.fn.fnamemodify(filename, ":t")
+        return filename
+    end,
+}
+
+local TablineFileNameBlock = {
+    init = function(self)
+        self.filename = vim.api.nvim_buf_get_name(self.bufnr)
+        local extension = vim.fn.fnamemodify(self.filename, ":e")
+        self.fileicon, self.icon_color = require("nvim-web-devicons").get_icon_color(self.filename, extension, { default = true })
+    end,
+    hl = function(self)
+        if self.is_active then
+            return
+        else
+            return { fg = "none" }
+        end
+    end,
+    on_click = {
+        callback = function(_, minwid, _, button)
+            if (button == "m") then -- close on mouse middle click
+                vim.api.nvim_buf_delete(minwid, {force = false})
+            else
+                vim.api.nvim_win_set_buf(0, minwid)
+            end
+        end,
+        minwid = function(self)
+            return self.bufnr
+        end,
+        name = "heirline_tabline_buffer_callback",
+    },
+    { provider = "▎  ", hl = { fg = "cyan" } },
+    TablineFileIcon,
+    { provider = " " },
+    TablineFileName,
+    TablineFileFlags,
+}
+
+local TreeOffset = {
+    condition = function(self)
+        local win = vim.api.nvim_tabpage_list_wins(0)[1]
+        local bufnr = vim.api.nvim_win_get_buf(win)
+        self.winid = win
+
+        if vim.bo[bufnr].filetype == "NvimTree" then
+            self.title = "File Explorer"
+        end
+        return vim.bo.filetype == "NvimTree"
+    end,
+
+    provider = function(self)
+        local title = self.title
+        local width = vim.api.nvim_win_get_width(self.winid)
+        local pad = math.ceil((width - #title) / 2)
+        return string.rep(" ", pad-1) .. title .. string.rep(" ", pad-1)
+    end,
+
+    hl = { fg = "none" }
+}
+
+local BufferLine = utils.make_buflist(
+    TablineFileNameBlock,
+    {
+        condition = function()
+            return vim.bo.filetype ~= "NvimTree"
+        end,
+        provider = "", hl = { fg = "grey" }
+    },
+    {
+        condition = function()
+            return vim.bo.filetype ~= "NvimTree"
+        end,
+        provider = "", hl = { fg = "grey" }
+    }
+)
+
+local tabline = {
+    condition = function()
+        return vim.bo.buftype ~= "terminal"
+    end,
+    TreeOffset,
+    BufferLine
+}
+
+heirline.setup(statusline, tabline)
